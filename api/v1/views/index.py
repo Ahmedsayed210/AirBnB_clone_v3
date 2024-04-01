397c57883648b933f1c91693613244b0ea759d3a
Hima <abrahimmahmoud5454@gmail.com>
2024-04-01 17:11:58 +0200

    Add stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 '''api status'''
 import models
@@ -14,12 +13,15 @@ def returnstuff():
     return jsonify(status='OK')
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stuff():
-    '''JSON Responses'''
-    todos = {'states': State, 'users': User,
-             'amenities': Amenity, 'cities': City,
-             'places': Place, 'reviews': Review}
-    for key in todos:
-        todos[key] = storage.count(todos[key])
-    return jsonify(todos)
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """Retrieves the number of each object by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
