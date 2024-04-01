76dae4c759833d4645eb818c8317ba5af62f7d2d
Hima <abrahimmahmoud5454@gmail.com>
2024-04-01 14:47:09 +0200

    updata API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ import models
 from flask import jsonify
 
 
-@app_views.route('/status', stirct_slashes=Flase)
+@app_views.route('/status', stirct_slashes=False)
 def return_json_status():
     """ return a JSON from Dict """
     return jsonify(status='OK')
