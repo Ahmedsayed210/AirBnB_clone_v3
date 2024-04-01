1880c37dcb8928150269910188aafd74ef2fea84
Hima <abrahimmahmoud5454@gmail.com>
2024-04-01 14:52:55 +0200

    updata API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 from api.v1.views.index import *
-from flask import Blueprints
+from flask import Blueprint
 
-app_views = Blueprints('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
