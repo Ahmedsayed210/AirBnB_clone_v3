2308270bceddcf9929dfebcd4abc42610bf03d7f
AhmedSayed210 <romaahmed791@gmail.com>
2024-04-02 04:14:27 +0200

    8-pycode

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -6,13 +6,17 @@ from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
 
+
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
     ''' Retrieve all Amenity objects '''
-    amenities = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
+    amenities = [amenity.to_dict() for amenity in
+                 storage.all(Amenity).values()]
     return jsonify(amenities)
 
-@app_views.route('/amenities/<string:amenity_id>', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/amenities/<string:amenity_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
     ''' Retrieve a Amenity object by id '''
     amenity = storage.get(Amenity, amenity_id)
@@ -20,7 +24,9 @@ def get_amenity(amenity_id):
         abort(404)
     return jsonify(amenity.to_dict())
 
-@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/amenities', methods=['POST'],
+                 strict_slashes=False)
 def create_amenity():
     ''' Create a new Amenity object '''
     data = request.get_json()
@@ -33,7 +39,9 @@ def create_amenity():
     storage.save()
     return jsonify(new_amenity.to_dict()), 201
 
-@app_views.route('/amenities/<string:amenity_id>', methods=['PUT'], strict_slashes=False)
+
+@app_views.route('/amenities/<string:amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     ''' Update a Amenity object '''
     amenity = storage.get(Amenity, amenity_id)
@@ -49,7 +57,9 @@ def update_amenity(amenity_id):
     storage.save()
     return jsonify(amenity.to_dict()), 200
 
-@app_views.route('/amenities/<string:amenity_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/amenities/<string:amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     ''' Delete a Amenity object '''
     amenity = storage.get(Amenity, amenity_id)
