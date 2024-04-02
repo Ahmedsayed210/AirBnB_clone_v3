a111de9f4e60ad7ad2c3774d88b6b82e187d809d
AhmedSayed210 <romaahmed791@gmail.com>
2024-04-02 04:08:26 +0200

    pycodestyle

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -14,7 +14,8 @@ def get_users():
     return jsonify(users)
 
 
-@app_views.route('/users/<string:user_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/users/<string:user_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_user(user_id):
     ''' Retrieve a User object by id '''
     user = storage.get(User, user_id)
@@ -23,7 +24,8 @@ def get_user(user_id):
     return jsonify(user.to_dict())
 
 
-@app_views.route('/users', methods=['POST'], strict_slashes=False)
+@app_views.route('/users',
+                 methods=['POST'], strict_slashes=False)
 def create_user():
     ''' Create a new User object '''
     data = request.get_json()
@@ -39,7 +41,8 @@ def create_user():
     return jsonify(new_user.to_dict()), 201
 
 
-@app_views.route('/users/<string:user_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/users/<string:user_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
     ''' Update a User object '''
     user = storage.get(User, user_id)
@@ -56,7 +59,8 @@ def update_user(user_id):
     return jsonify(user.to_dict()), 200
 
 
-@app_views.route('/users/<string:user_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/users/<string:user_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
     ''' Delete a User object '''
     user = storage.get(User, user_id)
